03977b40f7b2d470b8f6f942129c49eae4b98bdb
Ly <626947388@qq.com>
2019-04-03 08:35:23 +0800

    完成antd和react-router-dom的配置

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
+
+import Login from './pages/login';
+import Admin from './pages/admin';
+
+export default class App extends Component {
+  render() {
+    return (
+      <Switch>
+        <Route path="/login" component={Login} />
+        <Route path="/" component={Admin} />
+        //为了开发login设计的
+        <Redirect to='/login'/>
+      </Switch>
+    )
+  }
+}
---
+++ src/pages/admin/index.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class Admin extends Component {
+  render() {
+    return (
+      <div>
+        <p>admin</p>
+      </div>
+    )
+  }
+}
