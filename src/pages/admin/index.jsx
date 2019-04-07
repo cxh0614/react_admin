b6d467bb2d4949528c6cd7d5d454a255bd53cf88
Ly <626947388@qq.com>
2019-04-07 12:17:26 +0800

    分类数据的动态展示

--- src/pages/admin/index.jsx
+++ src/pages/admin/index.jsx
@@ -34,12 +34,11 @@ export default class Admin extends Component {
 
     //判断用户是否登录
     const user = getItem();
-    if (!user || !user._id) {
-      //说明用户没有登录，跳转到登录页面
-      return this.props.history.replace('/login');
+    if (user && user._id) {
+       //在内存中存储用户信息
+      memory.user = user;
     }
-    //在内存中存储用户信息
-    memory.user = user;
+   
   }
 
   onCollapse = (collapsed) => {
@@ -48,6 +47,11 @@ export default class Admin extends Component {
   }
 
   render() {
+    //说明用户没有登录，跳转到登录页面
+    if (!memory.user || !memory.user._id) {
+      return <Redirect to='/login' />
+    }
+
     const { collapsed } = this.state;
     const opacity = collapsed ? 0 : 1;
 
